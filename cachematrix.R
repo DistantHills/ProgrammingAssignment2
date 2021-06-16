57184bfaea2ccd791f3ffba5c62b07194248d18b
DistantHills <estella.warwick@gmail.com>
2021-06-16 17:20:33 +0100

    Fill in the 2 required matrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2
+## Provide the inverse of a matrix, using a cache so that 
+## we don't have to recalculate each time.
+##
+## EW June 2021
 
-## Write a short comment describing this function
-
+## Provide get/set access functions for a given matrix 
+## so that values can be cached.
+## returns  - List with elements/functions
+##              - set() & get() for the matrix
+##              - setInverse() & getInverse() for the inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    cacheInverse <- NULL
+    set <- function(newMatrix) {
+        x <<- newMatrix
+        cacheInverse <<- NULL
+    }
+    get <- function() x
+    getInverse <- function() cacheInverse
+    setInverse <- function(newInverse) cacheInverse <<- newInverse
+    list(get = get, set = set, 
+         getInverse = getInverse, setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return the inverse of a matrix, using the cached value if possible
+## cm    - a list of access functions for matrix, provided by makeCacheMatrix()
+cacheSolve <- function(cm, ...) {
+    ## WIBNI check cm is valid, before we start doing anything
+    if (is.null(cm$getInverse())) {
+        ## No cached value, so need to calculate & store it
+        message("Note - calculating new inverse")
+        cm$setInverse(solve(cm$get()))
+    }
+    cm$getInverse()
 }
